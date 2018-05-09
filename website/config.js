2d2d958b67bd9cc59b3ac0efe7b97d2dd574cbb7
Mika Lindqvist <postmaster@raasu.org>
2018-05-09 17:23:30 +0300

    Update website/config.js * Fix URLs for blockchainExplorer and transactionExplorer * Update pool list

--- website/config.js
+++ website/config.js
@@ -12,16 +12,15 @@ var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://poolhost.com/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://poolhost.com/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "pstar": [
         ["pstar.poolmining.us", "http://pstar.poolmining.us:7111"],
-        ["pinkpool.ga", "http://pinkpool.ga:8117"],
-        ["pool.ahka.net/pinkstarcoin/", "http://pnks.pool.ahka.net:8114"]
+        ["npool.pw/pstar", "http://ca.npool.pw:8114"]
     ]
 };
