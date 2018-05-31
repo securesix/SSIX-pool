5d85148c9ad3fb8172a0788502cc134d60252f84
Mika Lindqvist <postmaster@raasu.org>
2018-05-31 21:51:16 +0300

    Update website/config.js * Default blockchain and transaction explorer links now point to explorer.pinkstarcoin.com * Change poolmining.us to pool.pinkstarcoin.com * Link to English frontend of pstar.5wa.io

--- website/config.js
+++ website/config.js
@@ -1,6 +1,6 @@
 var api = "http://poolhost.com:8117";
 
-var api_blockexplorer = "http://daemonhost.com:39984";
+var api_blockexplorer = "http://pool.pinkstarcoin.com:39984";
 
 var poolHost = "poolhost.com";
 
@@ -12,16 +12,16 @@ var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://poolhost.com/?hash={id}#blockchain_block";
+var blockchainExplorer = "http://explorer.pinkstarcoin.com/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://poolhost.com/?hash={id}#blockchain_transaction";
+var transactionExplorer = "http://explorer.pinkstarcoin.com/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "pstar": [
-        ["poolmining.us", "http://poolmining.us:7111/"],
-        ["npool.pw/pstar", "http://ca.npool.pw:8114/"],
-        ["pstar.5wa.io", "http://pstar.5wa.io:8117/"]
+        ["pool.pinkstarcoin.com", "http://pool.pinkstarcoin.com:7111"],
+        ["npool.pw/pstar", "http://ca.npool.pw:8114"],
+        ["pstar.5wa.io/en", "http://pstar.5wa.io:8117"]
     ]
 };
