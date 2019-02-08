5a705e400fbb8302926ddaf59724cd6bd6242022
Mika Lindqvist <postmaster@raasu.org>
2019-02-08 22:46:43 +0200

    Show elapsed days in round time * Update pool list

--- website/config.js
+++ website/config.js
@@ -21,10 +21,14 @@ var themeCss = "themes/default-theme.css";
 var networkStat = {
     "btor": [
         ["btor.simpool.xyz", "https://btor.simpool.xyz:4119"],
-        ["youpool.io/BTOR", "http://144.202.120.143:8119"],
-        ["miningpool.studio","https://miningpool.studio:8119"],
+        ["youpool.io/BTOR", "http://144.202.120.172:8119"],
         ["pool.bittorium.org","https://pool.bittorium.org:8119"],
         ["btor.thepiratemine.nl", "https://btor.thepiratemine.nl:2896"],
-        ["pool.raasu.org", "https://pool.raasu.org:8119"]
+        ["pool.raasu.org", "https://pool.raasu.org:8119"],
+        ["disjampools.us/btor","https://disjampools.us:9004"],
+        ["coinpoolit.webhop.me/btor","https://ns3128504.ip-51-68-204.eu:7119"],
+        ["cryptoknight.cc/bittorium","https://cryptoknight.cc/rpc/btor"],
+        ["btor.cryptomine.cx","https://btor.cryptomine.cx:8007"],
+        ["btor.cryptocoins-digging.space","https://btor.cryptocoins-digging.space:8009"]
     ]
 };
