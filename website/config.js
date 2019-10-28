d6a3e62d5d24201345f392f4a21f957a1984f471
Mika Lindqvist <postmaster@raasu.org>
2019-10-28 13:32:58 +0200

    Initial version.

--- website/config.js
+++ website/config.js
@@ -1,6 +1,6 @@
 var api = "http://poolhost.com:8117";
 
-var api_blockexplorer = "https://explorer.bittorium.org/api";
+var api_blockexplorer = "https://explorer.talleo.org/api";
 
 var poolHost = "poolhost.com";
 
@@ -12,23 +12,15 @@ var email = "support@poolhost.com";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
-var blockchainExplorer = "https://explorer.bittorium.org/?hash={id}#block";
+var blockchainExplorer = "https://explorer.talleo.org/?hash={id}#block";
 
-var transactionExplorer = "https://explorer.bittorium.org/?hash={id}#transaction";
+var transactionExplorer = "https://explorer.talleo.org/?hash={id}#transaction";
 
 var themeCss = "themes/default-theme.css?ver=1";
 
 var networkStat = {
-    "btor": [
-        ["btor.simpool.xyz", "https://btor.simpool.xyz:4119"],
-        ["youpool.io/BTOR", "http://144.202.120.172:8119"],
-        ["pool.bittorium.org","https://pool.bittorium.org:8119"],
-        ["btor.thepiratemine.nl", "https://btor.thepiratemine.nl:2896"],
-        ["pool.raasu.org", "https://pool.raasu.org:8119"],
-        ["disjampools.us/btor","https://disjampools.us:9004"],
-        ["coinpoolit.webhop.me/btor","https://ns3128504.ip-51-68-204.eu:7119"],
-        ["cryptoknight.cc/bittorium","https://cryptoknight.cc/rpc/btor"],
-        ["btor.cryptomine.cx","https://btor.cryptomine.cx:8007"],
-        ["btor.cryptocoins-digging.space","https://btor.cryptocoins-digging.space:8009"]
+    "tlo": [
+        ["pool.talleo.org","https://pool.talleo.org:7112"],
+        ["tlo-pool.raasu.org","https://tlo-pool.raasu.org:7119"]
     ]
 };
